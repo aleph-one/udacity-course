375c6fea6dd187462c41b8fc4fae9fea6673a565
Andreas <andreas.mitter@gmail.com>
2019-05-14 11:08:35 +0200

    Stateful Widgets

--- course/04_navigation/task_04_navigation/lib/category.dart
+++ course/04_navigation/task_04_navigation/lib/category.dart
@@ -48,6 +48,18 @@ class Category extends StatelessWidget {
   /// Navigates to the [ConverterRoute].
   void _navigateToConverter(BuildContext context) {
     // TODO: Using the Navigator, navigate to the [ConverterRoute]
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              elevation: 1.0,
+              title: Text(this.name),
+              centerTitle: true,
+              backgroundColor: this.color,
+            ),
+            body: ConverterRoute(this.units, this.name, this.color),
+          );
+    }));
   }
 
   /// Builds a custom widget that shows [Category] information.
@@ -71,7 +83,7 @@ class Category extends StatelessWidget {
           // syntax.
           // TODO: Update this onTap property to call _navigateToConverter()
           onTap: () {
-            print('I was tapped!');
+            _navigateToConverter(context);
           },
           child: Padding(
             padding: EdgeInsets.all(8.0),
--- course/04_navigation/task_04_navigation/lib/converter_route.dart
+++ course/04_navigation/task_04_navigation/lib/converter_route.dart
@@ -16,12 +16,16 @@ import 'package:task_04_navigation/unit.dart';
 class ConverterRoute extends StatelessWidget {
   /// Units for this [Category].
   final List<Unit> units;
+  final String name;
+  final Color color;
 
   /// This [ConverterRoute] requires the name, color, and units to not be null.
   // TODO: Pass in the [Category]'s name and color
-  const ConverterRoute({
-    @required this.units,
-  }) : assert(units != null);
+  const ConverterRoute(
+    this.units,
+    this.name,
+    this.color
+  ) : assert(units != null);
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +33,7 @@ class ConverterRoute extends StatelessWidget {
     final unitWidgets = units.map((Unit unit) {
       // TODO: Set the color for this Container
       return Container(
+        color: this.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
